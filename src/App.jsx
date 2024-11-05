71c2016a665deb295efc628906748dc5cea272b6
MurawskiC1 <carter.murawski@spartans.ut.edu>
2024-11-05 11:33:05 -0500

    list bursts

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import { generateClient } from "aws-amplify/data";
 import { Amplify } from 'aws-amplify';
 import outputs from '../amplify_outputs.json';
-
+import data from './pulse_shape.json';
 
 Amplify.configure(outputs);
 
@@ -35,9 +35,19 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>GRB Burst Data</h1>
+      <ul>
+        {data.map((burst) => (
+          <li key={burst.BurstID}>
+            <h2>{burst.Burst_Name}</h2>
+            <p><strong>Burst ID:</strong> {burst.BurstID}</p>
+            <p><strong>Simple:</strong> {burst.Simple}</p>
+            <p><strong>Extended:</strong> {burst.Extended}</p>
+            <p><strong>Final Confidence:</strong> {burst.Final_Confidence}</p>
+            <img src={burst.Burst_PNG} alt={`${burst.Burst_Name} image`} />
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
