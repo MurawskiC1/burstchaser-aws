b53ea9634433703a52a1eded3124602875d71c67
MurawskiC1 <carter.murawski@spartans.ut.edu>
2024-11-05 11:16:45 -0500

    no Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './App.css';
-import data from './pulse_shape.json';
+
 
 
 function App() {
@@ -11,18 +11,7 @@ function App() {
     <>
       <div>
         <h1>GRB Burst Data</h1>
-        <ul>
-          {data.map((burst) => (
-            <li key={burst.BurstID}>
-              <h2>{burst.Burst_Name}</h2>
-              <p><strong>Burst ID:</strong> {burst.BurstID}</p>
-              <p><strong>Simple:</strong> {burst.Simple}</p>
-              <p><strong>Extended:</strong> {burst.Extended}</p>
-              <p><strong>Final Confidence:</strong> {burst.Final_Confidence}</p>
-              <img src={burst.Burst_PNG} alt={`${burst.Burst_Name} image`} />
-            </li>
-          ))}
-        </ul>
+
       </div >
     </>
   );
