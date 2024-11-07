a1d3915477649bb3d24429093585631b76c23430
MurawskiC1 <carter.murawski@spartans.ut.edu>
2024-11-07 15:59:01 -0500

    no break 2?

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { generateClient } from "aws-amplify/data";
-import { Amplify } from 'aws-amplify';
 
+import './App.css'
+/*import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';*/
 import data from './data/pulse_shape.json';
 
-Amplify.configure(outputs);
+/*Amplify.configure(outputs);*/
 
 function App() {
-  const [count, setCount] = useState(0)
 
 
 
@@ -18,23 +14,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+
       <h1>GRB Burst Data</h1>
       <ul>
         {data.map((burst) => (
