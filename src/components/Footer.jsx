ad8c7c34965f85ca01d984b7e3e5679964033148
MurawskiC1 <carter.murawski@spartans.ut.edu>
2025-01-16 12:58:22 -0500

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,43 +5,45 @@ export default function Footer(props) {
 
     return (
         <div>
-            <footer className="footer">
-                <div className="footerimage">
+            <footer>
+                <div className="footer">
+                    <div className="footerimage">
 
-                    <img src="burst_chaser.png" />
+                        <img src="burst_chaser.png" />
 
 
-                </div>
-                <div className="footer-text">
-                    <div className="options-head">
-                        <h1>Burst Chaser</h1>
-                        <p>A citizen science project aiming to
-                            unveil the mysteries about the most
-                            energetic explosions in the universe,
-                            Gamme Ray Bursts.
-                        </p>
-                    </div >
-                    <div className="footer-options">
+                    </div>
+                    <div className="footer-text">
+                        <div className="options-head">
+                            <h1>Burst Chaser</h1>
+                            <p>A citizen science project aiming to
+                                unveil the mysteries about the most
+                                energetic explosions in the universe,
+                                Gamme Ray Bursts.
+                            </p>
+                        </div >
+                        <div className="footer-options">
 
-                        <div className="options-options">
-                            <Link to="/"><p>About</p></Link>
-                            <Link to="data"><p>Explore</p></Link>
-                            <Link to="download"><p>Download</p></Link>
+                            <div className="options-options">
+                                <Link to="/"><p>About</p></Link>
+                                <Link to="data"><p>Explore</p></Link>
+                                <Link to="download"><p>Download</p></Link>
+                            </div>
+                            <div className="options-options">
+
+                                <a href="https://www.zooniverse.org/projects/amylien/burst-chaser/classify"><p>Classify Bursts</p></a >
+                                <a href="https://www.zooniverse.org/projects/amylien/burst-chaser"><p>Zooniverse</p></a >
+                                <p>Join Burst Chaser</p>
+
+                            </div>
+                            <div className="options-options">
+                                <a href="mailto:murawskiC1C4@gmail.com"><p>Contact</p></a>
+
+                            </div>
+
+
+
                         </div>
-                        <div className="options-options">
-
-                            <a href="https://www.zooniverse.org/projects/amylien/burst-chaser/classify"><p>Classify Bursts</p></a >
-                            <a href="https://www.zooniverse.org/projects/amylien/burst-chaser"><p>Zooniverse</p></a >
-                            <p>Join Burst Chaser</p>
-
-                        </div>
-                        <div className="options-options">
-                            <a href="mailto:murawskiC1C4@gmail.com"><p>Contact</p></a>
-
-                        </div>
-
-
-
                     </div>
                 </div>
                 <div className="trademark">
